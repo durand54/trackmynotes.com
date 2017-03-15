013d9621affef8d0d122e0f40a74fc6fd2fce311
durand54 <durandjones@inskyle.com>
2017-03-15 15:51:15 -0700

    change the title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Track My Notes
                 </div>
 
                 <div class="links">
